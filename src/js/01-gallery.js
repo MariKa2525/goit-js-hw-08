624cff666da04719593aab1d53a73247efe08b66
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2022-12-08 23:53:29 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardsMarkup)
+
+function createGalleryCardsMarkup(galleryItems) {
+return galleryItems.map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href = "${original}">
+            <img
+                class = "gallery__image"
+                src = "${preview}"
+                data-source = "${original}"
+                alt = "${description}"
+            />
+        </a>
+    `;
+    }).join('');
+}
+
+new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from 'lodash.throttle'
 
+
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('.feedback-form textarea')
+
+form.addEventListener('submit', onFormSubmit);
+textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+const formData = {};
+console.log(formData);
+
+form.addEventListener('input', e => {
+    formData[e.target.name] = e.target.value;
+    
+});
+
+populateTextarea();
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+
+    localStorage.setItem('feedback-form-state', message);
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem('feedback-form-state');
+
+    if (savedMessage) {
+        textarea.value = savedMessage;
+    }
+}
+// const email = evt.target.value;
+
+// localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
+// const saveData = localStorage.getItem('feedback-form-state');
+// const parseDate = JSON.parse(saveData)
+
+
+
+// Відстежуй на формі подію input, і щоразу записуй у 
+//локальне сховище об'єкт з полями email і message, 
+//у яких зберігай поточні значення полів форми. 
+//Нехай ключем для сховища буде рядок "feedback-form-state".
+
+// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
+// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
+// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
