c4d8f0fb16f2d28b195593b83d104179cfb99d94
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2022-12-09 12:41:34 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,18 @@
 import throttle from 'lodash.throttle';
 import Player from "@vimeo/player";
 
-    const iframe = document.querySelector('#vimeo-player');
-    const player = new Player(iframe);
-    
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
 
-    const onPlay = function({ seconds }) {
-        localStorage.setItem("videoplayer-current-time", JSON.stringify(seconds))
-    };
-    
-    player.setCurrentTime(JSON.parse(localStorage.getItem("videoplayer-current-time") || '0'))
 
-    player.on('timeupdate', throttle(onPlay, 1000));
+
+const onPlay = function ({ seconds }) {
+    localStorage.setItem("videoplayer-current-time", seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"))
+
+
 
