202639090ebf3212c35a5a6be91b636ce2fc0649
MariKa2525 <113701062+MariKa2525@users.noreply.github.com>
2022-12-09 15:41:57 +0200

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,15 @@
 import throttle from 'lodash.throttle'
 
-
 const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('.feedback-form textarea')
-
+const textarea = document.querySelector('.feedback-form textarea');
+const input = document.querySelector('.feedback-form input');
+console.log(input);
 form.addEventListener('submit', onFormSubmit);
+input.addEventListener('input', throttle(onTextareaInput, 500));
 textarea.addEventListener('input', throttle(onTextareaInput, 500));
+// Відстежуй на формі подію input, і щоразу записуй у 
+//локальне сховище об'єкт з полями email і message, 
+//у яких зберігай поточні значення полів форми. 
 
 const formData = {};
 console.log(formData);
@@ -25,18 +29,59 @@ function onFormSubmit(evt) {
 }
 
 function onTextareaInput(evt) {
-    const message = evt.target.value;
+    const email = evt.currentTarget.value;
+    const message = evt.currentTarget.value;
 
-    localStorage.setItem('feedback-form-state', message);
+    localStorage.setItem('feedback-form-state', JSON.stringify(evt));
 }
 
 function populateTextarea() {
-    const savedMessage = localStorage.getItem('feedback-form-state');
+    const saveData = localStorage.getItem('feedback-form-state');
+    const parseDate = JSON.parse(saveData)
 
-    if (savedMessage) {
-        textarea.value = savedMessage;
+    console.log(parseDate);
+
+    if (parseDate) {
+        input.value = parseDate.email;
+        textarea.value = parseDate.message;
+        console.log(parseDate.email);
+        console.log(parseDate.message);
     }
 }
+
+
+//---------------
+
+// const formData = {};
+// console.log(formData);
+
+// form.addEventListener('input', e => {
+//     formData[e.target.name] = e.target.value;
+    
+// });
+
+// populateTextarea();
+
+// function onFormSubmit(evt) {
+//     evt.preventDefault();
+
+//     evt.currentTarget.reset();
+//     localStorage.removeItem('feedback-form-state');
+// }
+
+// function onTextareaInput(evt) {
+//     const message = evt.target.value;
+
+//     localStorage.setItem('feedback-form-state', message);
+// }
+
+// function populateTextarea() {
+//     const savedMessage = localStorage.getItem('feedback-form-state');
+
+//     if (savedMessage) {
+//         textarea.value = savedMessage;
+//     }
+// }
 // const email = evt.target.value;
 
 // localStorage.setItem('feedback-form-state', JSON.stringify({ email, message }));
